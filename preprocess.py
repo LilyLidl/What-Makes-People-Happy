e130955af0005b73a2ed45b3bbbc6661309902b1
LilyLidl <lilylee19960516@gmail.com>
2017-04-29 17:29:59 +0800

    reset

--- preprocess.py
+++ preprocess.py
@@ -69,6 +69,41 @@ def transform(filename):
     return {'data':df.as_matrix(),'target':df["Happy"].as_matrix()} 
     #return {'data':data,'target':target}
 
+def getMean(col):
+    col_sum = np.sum(np.nan_to_num(col))
+    col_count = np.sum(np.isnan(col))
+    if col_count != 0:
+        return 1.0*col_sum/col_count
+    else:
+        return 0
+
+def getMedian(col):
+    if col.shape[0] <= 0:
+        return 0
+    else:
+        return np.median(col)
+
+def getMostFrequent(col):
+    if col.shape[0] <= 0:
+        return 0
+    # Count and record occurance of all the values
+    counter = dict()
+    for entry in col:
+        if not counter.has_key(entry):
+            counter[entry] = 0
+        else:
+            counter[entry] += 1
+    # Find the most frequent value
+    vals = counter.keys()
+    occur = 0
+    fre_val = 0
+    for val in vals
+        if counter[val] > occur:
+            fre_val = val
+            occur = counter[val]
+    return fre_val
+
+
 def fill_missing(X, strategy, isClassified):
     """
      @X: input matrix with missing data filled by nan
@@ -79,6 +114,31 @@ def fill_missing(X, strategy, isClassified):
      otherwise, just take the median/mean/most_frequent values of input data to
      fill in the missing data
     """
+    
+    
     """ your code here """
+    (n, m) = X.shape
+    col = X[:,i]
+    
+    if isClassified == false:
+        for i in xrange(m):
+            if strategy == 'median':
+                sub_val = getMedian(col)
+            if strategy == 'mean':
+                sub_val = getMean(col)
+            if strategy == 'most_frequent'
+                sub_val = getMostFrequent(col)
+            X[:,i] = np.nan_to_num(sub_val)
+
+    else:
+        gender_col_index = 2
+        edu_col_index = 5
+        edu_max = max(X[:,edu_col_index])
+        for gen in xrange(2):
+            for edu in xrange(edu_max+1):
+                gen_indexes = np.where(X[:,gender_col_index] == gen)
+                edu_indexes = np.where(X[:,edu_col_index] == edu)
+                
+
 
     return X_full
