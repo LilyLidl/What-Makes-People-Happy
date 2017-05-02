a490d10fc6f77f0e04e426d6bdd9d672f8275aa6
Neo Niu <neoxyniu@gmail.com>
2017-05-02 18:56:44 +0800

    PCA_subplot not completed

--- plot/PCA_biplot.py
+++ plot/PCA_biplot.py
@@ -1,8 +1,11 @@
 import numpy as np
+import sys
+sys.path.append('../')
 from preprocess import transform
 from preprocess import fill_missing
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 filename_train = '../data/train.csv'
 train_dataset = transform(filename_train)
@@ -14,3 +17,21 @@ X_full, discard_row = fill_missing(X, 'most_frequent', False)
 y = np.delete(y,discard_row)
 print X_full
 
+pca = PCA(n_components=2)
+components = pca.components_
+# The first principle component:
+component1 = components[0,:]
+component2 = components[1,:]
+
+
+PC1_val = np.dot(X_full, component1)
+PC2_val = np.dot(X_full, component2)
+
+
+
+fig = plt.figure()
+fig.suptitle("PCA biplot")
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.scatter(PC1_val,PC2_val)
+fig.savefig("PCA_biplot.jpg")
